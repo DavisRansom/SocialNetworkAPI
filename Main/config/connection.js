f8c402ae1938d49b606aa4f48f96079ce0deefdc
DavisRansom <davisransom.vb@gmail.com>
2022-03-10 14:34:08 -0800

    Brought in Mongoose

--- Main/config/connection.js
+++ Main/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmedia', {
+await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmedia', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
